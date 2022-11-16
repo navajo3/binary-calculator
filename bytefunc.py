5877cf79e3e6f9d8175f8e870c8e5760f50e0a3e
deez nuts <ov3rsights@protonmail.com>
2022-11-16 02:15:55 +0000

    commit

--- bytefunc.py
+++ bytefunc.py
@@ -1,8 +1,15 @@
-def byte():
+
+def byte(bit, result):
     if bit == 1:
         result = format(result, '08b')
+        print("Number in binary: ", result[:8])
+        result = hex(int(result, 2))
+        print("\nNumber in hexadecimal: ", result[2:])
     elif bit == 16:
         result = format(result, '0128b')
+        print("Number in binary: ", result[:128])
+        result = hex(int(result, 2))
+        print("\nNumber in hexadecimal: ", result[2:])
     else:
         print("Code malfunction: Invalid bit value. Blame the programmer.")
         time.sleep(2)
